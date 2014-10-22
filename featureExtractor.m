982bc0b97a4e7f09d443e907d3d8198eca3f8d4e
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-10-22 13:28:38 +0200

    Small changes (resolution, etc.)

--- featureExtractor.m
+++ featureExtractor.m
@@ -1,30 +1,25 @@
-function [X]=featureExtractor()
-X = DrawCharacter;
+function featureSet = featureExtractor(charData)
 
 Xred = [];
 % Remove cols where third row has 0
 % Currently BUGGY for some reason
-for i = 1:length(X),
-    if X(3,i) == 1,
-        Xred(:,length(Xred)+1) = X(1:3,i);
+for i = 1:length(charData),
+    if charData(3,i) == 1,
+    Xred(:,length(Xred)+1) = charData(1:3,i);
     end
 end
 Xred = [Xred(1:3,1) Xred(1:3, 4:length(Xred))];
-XCatch = Xred;
 
 % Normalise position to O = (0.5, 0.5)
 averageX = sum(Xred(1,:)) / length(Xred);
 averageY = sum(Xred(2,:)) / length(Xred);
-
 dX = 0.5 - averageX;
 dY = 0.5 - averageY;
-
 Xred(1,:) = Xred(1,:) + dX;
 Xred(2,:) = Xred(2,:) + dY;
-
 xDeltaM = Xred(1,:);
 yDeltaM = Xred(2,:);
-
+%
 minst = min([xDeltaM yDeltaM]);
 xDeltaM = xDeltaM - minst;
 %minst = min(yDeltaM);
@@ -32,6 +27,12 @@ yDeltaM = yDeltaM - minst;
 norm = max(abs([xDeltaM yDeltaM]));
 xDeltaM = xDeltaM/norm;
 yDeltaM = yDeltaM/norm;
+featureSet = [xDeltaM; yDeltaM]';
+featureSet = sortrows(featureSet)';
 
-%plot(1:length(xDeltaM), round([xDeltaM;yDeltaM]*10));
-%axis([0 length(xDeltaM) 0 10]);
+RESOLUTION = 26;
+
+featureSet = round(featureSet*RESOLUTION);
+
+%plot(1:length(featureSet), featureSet);
+%axis([0 length(featureSet) 0 RESOLUTION]);
