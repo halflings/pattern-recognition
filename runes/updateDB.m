85eac5bb80456485f15fd7923ccb934e783357fd
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-10-28 13:09:54 +0100

    updateDB only gives the raw collection now, since features are done elsewhere

--- runes/updateDB.m
+++ runes/updateDB.m
@@ -1,25 +1,18 @@
-function [rawDB, featureDB] = updateDB(rawDB, featureDB, symbol, times)
+function rawDB = updateDB(rawDB, symbol, times)
 
 % Initialization, if it hasn't been done before
 if (isKey(rawDB, symbol) == 0)
     rawDB(symbol) = {};
 end
-if (isKey(featureDB, symbol) == 0)
-    featureDB(symbol) = {};
-end
 
 rdbs = rawDB(symbol);
-fdbs = featureDB(symbol);
 
 for i=1:times
     i
     raw = DrawCharacter();
-    features = featureExtractor(raw);
 
     N = length(rdbs);
     rdbs(N+1) = {raw};
-    fdbs(N+1) = {features};
 end
 
 rawDB(symbol) = rdbs;
-featureDB(symbol) = fdbs;
